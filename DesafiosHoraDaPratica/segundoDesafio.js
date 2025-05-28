633704f8ea9b2d29096d54c3fdf81835d2381b27
Mariana Souza <marsouzafranca@gmil.com>
2025-05-27 23:46:13 -0300

    Arrumando alguns erros de ortografia

--- DesafiosHoraDaPratica/segundoDesafio.js
+++ DesafiosHoraDaPratica/segundoDesafio.js
@@ -21,16 +21,16 @@ else{
 //3.Crie um sistema de pontuação para um jogo. Se a pontuação for maior ou igual a 100, mostre "Parabéns, você venceu!". Caso contrário, mostre "Tente novamente para ganhar.".
 let pontuacao = prompt('Insira sua pontuaçao');
 if(pontuacao >= 100){
-    alert('Parabens, voce venceu!');
+    alert('Parabéns, voce venceu!');
 }
 else{
-    alert('Voce perdeu, tente novamente para ganhar.');
+    alert('Você perdeu, tente novamente para ganhar.');
 }
 
 
 //4.Crie uma mensagem que informa o usuário sobre o saldo da conta, usando uma template string para incluir o valor do saldo.
 let saldoNaConta = prompt('Insira o seu saldo na conta:');
-alert(`O seu saldo disponivel na conta é de ${saldoNaConta}`);
+alert(`O seu saldo disponível na conta é de ${saldoNaConta}`);
 
 
 //5.Peça ao usuário para inserir seu nome usando prompt. Em seguida, mostre um alerta de boas-vindas usando esse nome.
